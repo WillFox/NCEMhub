ceb9317bd9347b8c4fac8081d472612004814d22
William Fox <wfox413@gmail.com>
2014-04-24 14:10:28 -0700

    Added changes for apache server (URL CHANGES NEEDED)

--- ncemhub/settings.py
+++ ncemhub/settings.py
@@ -8,7 +8,7 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
-
+URL_PREFIX='/ncemhub'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
@@ -65,7 +65,7 @@ MEDIA_ROOT = '/project/projectdirs/ncemhub/ncemhub/bin/ncemhub/www/media/'
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+MEDIA_URL = '/site_media/ncemhub/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
